e09a1f916e39165e251867d221fe81ea407011db
HammerKick <toitoinne@gmail.com>
2018-02-17 13:33:38 +0100

    nouvelle arborescence pour de vrai là

---
+++ php/demander_reprographie.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>IUT DE VELIZY - Reprographie: Demande</title>
+</head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<body>
+<nav class="nav navbar-default navbar-fixed-top">
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="Tindex.php">Accueil</a>
+  </li>
+  <li class="nav-item">
+	<a class="nav-link" href="se_connecter.php">Se connecter</a>
+	</li>
+  <li class="nav-item">
+    <a class="nav-link" href="demander_reprographie.php">Demander une impression</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="liste_impressions.php">Liste des impressions</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="statistique.php">Statistiques</a>
+  </li>
+</ul>
+</nav>
+
+<center>
+<div class="jumbotron">
+  <h1 class="display-5">Demander une impression</h1>
+  <p class="lead">Remplissez ce formulaire pour faire votre demande!</p>
+  </p>
+</div>
+
+<form method='post' action='reception.php' enctype='multipart/form-data'>
+  <div class="form-group">
+    <label for="nomDuFichier">Nom du document <input type="text" class="form-control" name="titre"></label>
+  </div>
+  
+  <div class="form-group">
+	<label for="fichier">Fichier à imprimer <strong>(formats acceptés : .pdf, .txt ; taille max : 1 Mo)</strong> <input type="file" class="form-control" name="mon_fichier"></label>
+  </div>
+  
+  <div class="form-group">
+	<label for="departement">Département
+	
+	<select class="form-control" name="departement">
+	<option>Informatique</option>
+	<option>MMI</option>
+	<option>GEII</option>
+	<option>Autre</option></label>
+	
+	</select>
+	</div>
+  
+  <div class="form-group">
+	<label for="options">
+	Couverture ? <input type="checkbox" name="couverture" id="couverture"><br>
+	<label><strong>Couleurs :</strong></label> <input type="radio" name="papier" id="papierblanc" value="1"> <label id="papierblanc">Papier blanc</label> <input type="radio" name="papier" id="papiercouleur" value="2"> Papier couleur<br>
+	<label><strong>Sens :</strong></label> <input type="radio" name="rectoverso" id="recto" value="1"> Recto seulement <input type="radio" name="rectoverso" id="lesdeux" value="2"> Recto/Verso
+	<br><br>
+	</label>
+	
+	<input type='hidden' name='MAX_FILE_SIZE' value='1048576' />
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</center>
---
+++ php/reception.php
@@ -0,0 +1,54 @@
+<?php
+$FichierValide = TRUE;
+$maxsize = $_POST['MAX_FILE_SIZE'];
+$message = "fichier bien envoyé";
+if(isset($_POST['couverture']))
+{
+	$couverture = $_POST['couverture'];
+}
+else
+{
+	$couverture = "none";
+}
+
+$papier = $_POST['papier'];
+$rectoverso = $_POST['rectoverso'];
+
+if ($_FILES['mon_fichier']['size'] > $maxsize) $message = "Le fichier est trop gros";
+
+$extensions_valides = array( 'txt' , 'pdf' );
+$extension_upload = strtolower(  substr(  strrchr($_FILES['mon_fichier']['name'], '.')  ,1)  );
+if (!in_array($extension_upload,$extensions_valides) ){$message = "mauvaise extension";}
+
+echo $message;
+echo "<br>";
+
+$id_membre = 'test';
+//$id_membre = $_POST[''];	//à changer afin de récupérer le nom du user
+$titre = $_POST['titre']; //contient le titre de l'envoi de l'utilisateurt
+
+echo "id membre : ".$id_membre;
+echo "<br>";
+echo "titre : ".$titre;
+echo "<br>";
+echo "couverture : ".$couverture;
+echo "<br>";
+echo "papier : ".$papier;	// !!! renvoit toujours on
+echo "<br>";
+echo "rectoverso : ".$rectoverso;	// !!! renvoit toujours on
+
+//******* Mettre ici l'envoi de données à la base
+
+/*
+//Relatif à l'ecriture dans un fichier
+
+if(!file_exists ( "fichier/{$id_membre}" ) )
+{
+	mkdir("fichier/{$id_membre}", 0777, true);
+}
+
+$nom = "fichier/{$id_membre}/{$titre}.{$extension_upload}";
+$resultat = move_uploaded_file($_FILES['mon_fichier']['tmp_name'],$nom);
+if ($resultat) echo "Transfert réussi";
+*/
+?>
